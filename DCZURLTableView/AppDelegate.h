1e258c2c8758ac897dd1cfe3485f4a0280cf16ad
Zaslow <dcz4@hl1-m52.labs.pitt.edu>
2015-04-22 03:19:27 -0400

    Initial Commit

---
+++ DCZURLTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DCZURLTableView
+//
+//  Created by Zaslow, Dean Clayton on 4/22/15.
+//  Copyright (c) 2015 Dean Zaslow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DCZURLTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DCZURLTableView
+//
+//  Created by Zaslow, Dean Clayton on 4/22/15.
+//  Copyright (c) 2015 Dean Zaslow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
